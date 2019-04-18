606c827d8197172efae0ddd63f16e904a94b9b45
mmorales4050 <mmorales4050@gmail.com>
2019-04-18 09:55:38 -0400

    add listing post

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,4 +3,14 @@ class ListingsController < ApplicationController
   def index
     render json: Listing.all
   end
+
+  def create
+    Listing.create(listing_params)
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:name, :description, :price, :image, :vendor_id)
+  end
 end
